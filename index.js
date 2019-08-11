caf42230a0e08e5b0b01d1f371683f62d2183e8f
Juan Gonzalez <3232rose@gmail.com>
2019-08-11 19:15:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 function theBeatlesPlay(musicians,instruments) {
   var array=[];
-  for (var i=0; i < musicians.length; i++) {
-    array.push(musicians[i] + "plays" + instruments[i]);
+  for (let i=0; i < musicians.length; i++) 
+  {
+    var string=`${musicians[i]} plays ${instruments[i]}`;
+    array.push(string);
   }
   return array;
 }
 
-  var facts=["He was the last Beatle to learn to drive","He was never a vegetarian","He was a choir boy and boy scout","He hated the sound of his own voice"];
  function johnLennonFacts(facts) {
   var newFacts=[];
-  var i=0;
-  while (i<4) {
-    newFacts[i] = facts[i] +="!!!";
+  let i=0;
+  while (i<facts.length) {
+    newFacts.push(`${facts[i]}!!!`);
     i++;
   }
   return newFacts; 
